0a22e98ca5c13e87ce1affdc697e8ce1502b4cfc
Aboshxm2 <72500560+Aboshxm2@users.noreply.github.com>
2021-02-11 09:36:43 +0300

    Update Room.php

--- src/Aboshxm2/privateroom/object/Room.php
+++ src/Aboshxm2/privateroom/object/Room.php
@@ -1,6 +1,6 @@
 <?php 
 /*
-Copyright [yyyy] [name of copyright owner]
+Copyright 2021-2022 Aboshxm2
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
